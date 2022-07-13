1bad0ddd990f8cc30e6eaaad551a199593a29b1b
Arnaud Delaunoy <delaunoy.arnaud@gmail.com>
2022-07-13 11:54:07 +0200

    comment models_util.py

--- models/models_util.py
+++ models/models_util.py
@@ -2,6 +2,18 @@ import os
 from .jax_models import get_model
 
 def load_model(dataset_name):
+    """Load the appropriate model for a given dataset
+
+    Args:
+        dataset_name (str): The name of the dataset
+
+    Returns:
+        tuple: (model, prior_variance, task)
+            model (tuple): A tuple containing the init and apply functions returned by haiku.transform
+			prior_variance (float): The weight's prior variance
+			task (str): The task, either "regression" or "classification"
+    """
+    
     if dataset_name == "cifar10":
         model = get_model("resnet20_frn_swish", data_info={"num_classes": 10})
         prior_variance = 1/5
